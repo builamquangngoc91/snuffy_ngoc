8a9fa329b62e4ce0bf03bb76842950daf5e1d5b4
zach <bui.lam.quang.ngoc@moneyforward.vn>
2025-02-14 23:13:47 +0700

    remove unused files

--- mae_adapter/timm_modified/loss/__init__.py
+++
@@ -1,3 +0,0 @@
-from .asymmetric_loss import AsymmetricLossMultiLabel, AsymmetricLossSingleLabel
-from .cross_entropy import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
-from .jsd import JsdCrossEntropy
